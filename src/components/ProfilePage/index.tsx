b6a13878689b9a4af9b9f25ea62a022f5732015d
bernardonascimento <bernardonasciimento@gmail.com>
2020-07-21 20:16:15 -0300

    Button, Feed, Layout, Main, ProfilePage, Tweet, styles globais e icons

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Feed from "../Feed";
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+
+        <h1>Bernardo Nascimento</h1>
+        <h2>@bernardonascimento</h2>
+
+        <p>
+          Developer at <a href="htttp://www.viscom.com.br">@ViscomTecnologia</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 20 de junho de 1991
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+
+      <Feed />
+    </Container>
+  );
+};
+
+export default ProfilePage;
